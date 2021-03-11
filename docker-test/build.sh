d6712691bfc5eb5b475fffda19132fd4feace6da
chevdor <chevdor@gmail.com>
2021-03-11 18:07:19 +0100

    Add testing image

---
+++ docker-test/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+RUSTC_VERSION=nightly-2021-02-25
+
+docker build -t chevdor/srtool-dev:$RUSTC_VERSION .
+
+echo You can now run:
+echo docker run --rm -it chevdor/srtool-dev:$RUSTC_VERSION build
+echo or
+echo docker run --rm -it -e SLEEP=0.01 chevdor/srtool-dev:$RUSTC_VERSION build
